6adcc7b0222e560b1c36d8988319f1aaf1a62f0b
theov <theov@theov-HP-ZBook-15>
2017-01-03 11:05:22 +0100

    Mise à jour de Calcul

--- src/main/java/exemples/Calcul.java
+++ src/main/java/exemples/Calcul.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Calcul {
 
     
-    public double calculTF(HashMap<String, HashMap<String,Integer>> hashmap,String mot, String document){
+  /**  public double calculTF(HashMap<String, HashMap<String,Integer>> hashmap,String mot, String document){
         double result = 0;
         if(hashmap.containsKey(document)){
             HashMap<String,Integer> temp = hashmap.get(document);
@@ -20,23 +20,6 @@ public class Calcul {
         return result;
     }
 
-    public double calculTF( LectureArbres lectureArbre,String mot, String document){
-        double result = 0;
-        LinkedList<String> listDoc = lectureArbre.chercherMot(mot);
-        for (String doc : listDoc){
-        	if (doc.equals(document)){
-        		result++ ;
-        	}
-        }
-        return result;
-    }
-
-    public double calculIDF(String mot, LectureArbres lectureArbre){
-        double result = 0;
-        Set<String> listDoc = new HashSet<String>(lectureArbre.chercherMot(mot));
-        result = Math.log10(listDoc.size()/result);
-        return result;
-    }
 
 
     public double calculIDF(HashMap<String, HashMap<String,Integer>> hashmap,String mot){
@@ -50,10 +33,28 @@ public class Calcul {
         result = Math.log10(listDoc.size()/result);
         return result;
     }
-    
-    public double calculTFIDF(HashMap<String, HashMap<String,Integer>> hashmap,String mot, String document){
+    **/
+    public double calculTF(LectureArbres lectureArbre,String mot, String document){
+        double result = 0;
+        LinkedList<String> listDoc = lectureArbre.chercherMot(mot);
+        for (String doc : listDoc){
+        	if (doc.equals(document)){
+        		result++ ;
+        	}
+        }
+        return result;
+    }
+
+    public double calculIDF(LectureArbres lectureArbre,String mot){
+        double result = 0;
+        Set<String> listDoc = new HashSet<String>(lectureArbre.chercherMot(mot));
+        result = Math.log10(listDoc.size()/result);
+        return result;
+    }
+
+    public double calculTFIDF(LectureArbres lectureArbre ,String mot, String document){
         double result ;
-        result = calculTF(hashmap,mot,document)*calculIDF(hashmap, mot);
+        result = calculTF(lectureArbre,mot,document)*calculIDF(lectureArbre, mot);
         return result;
     }
     
